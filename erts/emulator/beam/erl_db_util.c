sverker/otp

commit 40227c641ad468c481e9142fa09a227d323a013a

erts: Add asserts to suppress false positives found by clang sa

erts/emulator/beam/erl_db_util.c
@@ -143,8 +143,6 @@ do {									\
 } while (0)
 
 
-#define TermWords(t) (((t) / (sizeof(UWord)/sizeof(Eterm))) + !!((t) % (sizeof(UWord)/sizeof(Eterm))))
-
 #define add_dmc_err(EINFO, STR, VAR, TERM, SEV) \
        vadd_dmc_err(EINFO, SEV, VAR, STR, TERM)
 
@@ -1597,6 +1595,8 @@ Binary *db_match_compile(Eterm *matchexpr,
     heap.size = DMC_DEFAULT_SIZE;
     heap.vars = heap.vars_def;
 
+    ASSERT(num_progs > 0);
+
     /*
     ** Compile the match expression
     */
@@ -2226,7 +2226,7 @@ restart:
 		FAIL();
 	    if (sys_memcmp(float_val(*ep) + 1, pc, sizeof(double)))
 		FAIL();
-	    pc += TermWords(2);
+	    pc += FLOAT_SIZE_OBJECT;
 	    ++ep;
 	    break;
 	case matchEqRef: {
@@ -2237,7 +2237,7 @@ restart:
 		FAIL();
 	    }
 	    i = thing_arityval(*epc);
-	    pc += TermWords(i+1);
+	    pc += i+1;
 	    ++ep;
 	    break;
 	}
@@ -2250,7 +2250,7 @@ restart:
 		if (*tp != *epc)
 		    FAIL();
 		i = BIG_ARITY(epc);
-		pc += TermWords(i+1);
+		pc += i+1;
 		while(i--) {
 		    if (*++tp != *++epc) {
 			FAIL();
@@ -5323,8 +5323,10 @@ static Eterm match_spec_test(Process *p, Eterm against, Eterm spec, int trace)
 	if (trace) {
 	    if (n)
 		arr = erts_alloc(ERTS_ALC_T_DB_TMP, sizeof(Eterm) * n);
-	    else 
+	    else {
+                ASSERT(!is_list(against));
 		arr = NULL;
+            }
 	    l = against;
 	    n = 0;
 	    while (is_list(l)) {
@@ -5540,18 +5542,18 @@ void db_match_dis(Binary *bp)
 		if (is_ordinary_ref_thing(t)) {
 		    ErtsORefThing *rt = (ErtsORefThing *) t;
 		    num = rt->num;
-		    t += TermWords(ERTS_REF_THING_SIZE);
+		    t += ERTS_REF_THING_SIZE;
 		}
 		else if (is_pid_ref_thing(t)) {
 		    ErtsPRefThing *prt = (ErtsPRefThing *) t;
 		    num = prt->num;
-		    t += TermWords(ERTS_PID_REF_THING_SIZE);
+		    t += ERTS_PID_REF_THING_SIZE;
 		}
 		else {
 		    ErtsMRefThing *mrt = (ErtsMRefThing *) t;
 		    ASSERT(is_magic_ref_thing(t));
 		    num = mrt->mb->refn;
-		    t += TermWords(ERTS_MAGIC_REF_THING_SIZE);
+		    t += ERTS_MAGIC_REF_THING_SIZE;
 		}
 
 		erts_printf("EqRef\t(%d) {", (int) ERTS_REF_NUMBERS);
@@ -5575,7 +5577,7 @@ void db_match_dis(Binary *bp)
 	    n = thing_arityval(*t);
 	    {
 		Eterm *et = (Eterm *) t;
-		t += TermWords(n+1);
+		t += n+1;
 		erts_printf("EqBig\t(%d) {", (int) n);
 		first = 1;
 		++n;
@@ -5599,7 +5601,7 @@ void db_match_dis(Binary *bp)
 	    {
 		double num;
 		sys_memcpy(&num,t,sizeof(double));
-		t += TermWords(2);
+		t += FLOAT_SIZE_OBJECT;
 		erts_printf("EqFloat\t%f\n", num);
 	    }
 	    break;

erts/emulator/beam/beam_bif_load.c
@@ -77,13 +77,11 @@ init_purge_state(void)
 	erts_export_put(am_erts_code_purger, am_pending_purge_lambda, 3);
 
     purge_state.sprocs = &purge_state.def_sprocs[0];
-    purge_state.sp_size = sizeof(purge_state.def_sprocs);
-    purge_state.sp_size /= sizeof(purge_state.def_sprocs[0]);
+    purge_state.sp_size = sizeof(purge_state.def_sprocs) / sizeof(purge_state.def_sprocs[0]);
     purge_state.sp_ix = 0;
 
     purge_state.funs = &purge_state.def_funs[0];
-    purge_state.fe_size = sizeof(purge_state.def_funs);
-    purge_state.fe_size /= sizeof(purge_state.def_funs[0]);
+    purge_state.fe_size = sizeof(purge_state.def_funs) / sizeof(purge_state.def_funs[0]);
     purge_state.fe_ix = 0;
 
     purge_state.saved_old.code_hdr = 0;

erts/emulator/beam/beam_bp.c
@@ -718,7 +718,7 @@ erts_generic_breakpoint(Process* c_p, ErtsCodeInfo *info, Eterm* reg)
 	new_tracer = do_call_trace(c_p, info, reg, 1, bp->meta_ms, old_tracer);
 
 	if (!ERTS_TRACER_COMPARE(new_tracer, old_tracer)) {
-            if (old_tracer == erts_atomic_cmpxchg_acqb(
+            if ((erts_aint_t)old_tracer == erts_atomic_cmpxchg_acqb(
                     &bp->meta_tracer->tracer,
                     (erts_aint_t)new_tracer,
                     (erts_aint_t)old_tracer)) {
@@ -1129,12 +1129,15 @@ static void bp_hash_rehash(bp_time_hash_t *hash, Uint n) {
     Uint ix;
     Uint hval;
 
+    ASSERT(n > 0);
+
     item = (bp_data_time_item_t *)Alloc(size);
     sys_memzero(item, size);
 
     for( ix = 0; ix < n; ++ix) {
 	item[ix].pid = NIL;
     }
+
 
     /* rehash, old hash -> new hash */
 
@@ -1395,7 +1398,7 @@ set_function_break(ErtsCodeInfo *ci, Binary *match_spec, Uint break_flags,
 	bp->count = bcp;
     } else if (break_flags & ERTS_BPF_TIME_TRACE) {
 	BpDataTime* bdt;
-	int i;
+	Uint i;
 
 	ASSERT((bp->flags & ERTS_BPF_TIME_TRACE) == 0);
 	bdt = Alloc(sizeof(BpDataTime));

erts/emulator/beam/beam_debug.c
@@ -458,7 +458,7 @@ print_op(fmtfn_t to, void *to_arg, int op, int size, BeamInstr* addr)
 	 * Copy all arguments to a local buffer for the unpacking.
 	 */
 
-	ASSERT(size <= sizeof(args)/sizeof(args[0]));
+	ASSERT(size > 0 && size <= sizeof(args)/sizeof(args[0]));
 	ap = args;
 	for (i = 0; i < size; i++) {
 	    *ap++ = addr[i];
@@ -553,14 +553,14 @@ print_op(fmtfn_t to, void *to_arg, int op, int size, BeamInstr* addr)
 	    break;
         case 'S':               /* Register */
             {
-                Uint reg_type = (*ap & 1) ? 'y' : 'x';
+                Uint reg_type = (ap[0] & 1) ? 'y' : 'x';
                 Uint n = ap[0] / sizeof(Eterm);
                 erts_print(to, to_arg, "%c(%d)", reg_type, n);
 		ap++;
                 break;
             }
 	case 's':		/* Any source (tagged constant or register) */
-	    tag = loader_tag(*ap);
+	    tag = loader_tag(ap[0]);
 	    if (tag == LOADER_X_REG) {
 		erts_print(to, to_arg, "x(%d)", loader_x_reg_index(*ap));
 		ap++;

erts/emulator/beam/bif.c
@@ -1591,7 +1591,7 @@ static Eterm process_flag_aux(Process *c_p, int *redsp, Eterm flag, Eterm val)
        if (i == 0)
 	   scb = NULL;
        else {
-	   Uint sz = sizeof(*scb) + (i-1) * sizeof(scb->ct[0]);
+	   Uint sz = sizeof(*scb) + (i-1) * sizeof(*scb->ct);
 	   scb = erts_alloc(ERTS_ALC_T_CALLS_BUF, sz);
 	   scb->len = i;
 	   scb->cur = 0;

erts/emulator/beam/dist.c
@@ -2847,6 +2847,8 @@ erts_dsig_prepare(ErtsDSigSendContext *ctx,
 {
     int res;
 
+    ASSERT(no_trap || proc);
+
     if (!erts_is_alive)
 	return ERTS_DSIG_PREP_NOT_ALIVE;
     if (!dep) {

erts/emulator/beam/erl_async.c
@@ -137,6 +137,7 @@ static void *async_main(void *);
 static ERTS_INLINE ErtsAsyncQ *
 async_q(int i)
 {
+    ASSERT(async != NULL);
     return &async->queue[i].aq;
 }
 

erts/emulator/beam/erl_bif_re.c
@@ -1042,6 +1042,7 @@ build_capture(Eterm capture_spec[CAPSPEC_SIZE], const pcre *code)
 						    (tmpbsiz = ap->len + 1));
 			    }
 			}
+                        ASSERT(tmpb != NULL);
 			sys_memcpy(tmpb,ap->name,ap->len);
 			tmpb[ap->len] = '\0';
 		    } else {
@@ -1059,7 +1060,7 @@ build_capture(Eterm capture_spec[CAPSPEC_SIZE], const pcre *code)
 						    (tmpbsiz = slen + 1));
 			    }
 			}
-
+                        ASSERT(tmpb != NULL);
 			buffres = erts_iolist_to_buf(val, tmpb, slen);
 			ASSERT(buffres >= 0); (void)buffres;
 			tmpb[slen] = '\0';

commit adc01fe146529b3c7e42aa8d7e6822d37b0deb28

erts: Fix match spec bug in matchEqFloat on 32-bit
introduced by 40227c641ad468c481e9142fa09a227d323a013a.

Also remove superfluous 0-word for matchEqFloat on 64-bit.

erts/emulator/beam/erl_db_util.c
@@ -2226,7 +2226,7 @@ restart:
 		FAIL();
 	    if (sys_memcmp(float_val(*ep) + 1, pc, sizeof(double)))
 		FAIL();
-	    pc += FLOAT_SIZE_OBJECT;
+	    pc += sizeof(double) / sizeof(*pc);
 	    ++ep;
 	    break;
 	case matchEqRef: {
@@ -3689,9 +3689,7 @@ static DMCRet dmc_one_term(DMCContext *context,
 	}
 	case (_TAG_HEADER_FLOAT >> _TAG_PRIMARY_SIZE):
 	    DMC_PUSH2(*text, matchEqFloat, (Uint) float_val(c)[1]);
-#ifdef ARCH_64
-	    DMC_PUSH(*text, (Uint) 0);
-#else
+#ifdef ARCH_32
 	    DMC_PUSH(*text, (Uint) float_val(c)[2]);
 #endif
 	    break;
@@ -5601,7 +5599,7 @@ void db_match_dis(Binary *bp)
 	    {
 		double num;
 		sys_memcpy(&num,t,sizeof(double));
-		t += FLOAT_SIZE_OBJECT;
+		t += sizeof(double) / sizeof(*t);
 		erts_printf("EqFloat\t%f\n", num);
 	    }
 	    break;